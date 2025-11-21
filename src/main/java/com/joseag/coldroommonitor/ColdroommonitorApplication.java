de011d710b11192d530602c6a5da24880d3d4843
jose-aguilar <jesusaguilargarcia435@gmail.com>
2025-11-20 23:45:11 -0600

    First Commit

---
+++ src/main/java/com/joseag/coldroommonitor/ColdroommonitorApplication.java
@@ -0,0 +1,13 @@
+package com.joseag.coldroommonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColdroommonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColdroommonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joseag/coldroommonitor/ColdroommonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseag.coldroommonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColdroommonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
